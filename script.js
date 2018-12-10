ee94c3ba7d34aef19b8f8ff4879e04bc5ffdcd0e
Dennis <deweis@gmx.ch>
2018-12-10 12:44:16 +0100

    Adding a simple example for a bar chart

--- script.js
+++ script.js
@@ -4,9 +4,36 @@
 const dataset = [1, 2, 3, 4, 5];
 
 /* Create a p tag for each item in dataset */
-d3.select('body') // select the body element
+d3.select('#pExample') // select the body element
   .selectAll('p') // select all p tags -> will return an empty selection as no p elements available
   .data(dataset) // will put the data in waiting state for further processing
   .enter() // will take the data items one by one and perform further operations on them
   .append('p') // appends a p item for each data item
   .text(d => d); // binds respective text inside each p item (in this case the dataitem itself)
+
+/**************************************************************************************************
+  Example on creating a simple bar chart
+**************************************************************************************************/
+const barChartData = [80, 100, 56, 120, 180, 30, 40, 120, 160], // the dataset
+  svgWidth = 500, // the width of the svg container
+  svgHeight = 300, // the height of the svg container
+  barPadding = 5, // the padding between the bars
+  barWidth = svgWidth / barChartData.length; // give equal width to every bar based on the container width
+
+/* configure the svg container */
+const svg = d3
+  .select('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
+
+const barChart = svg
+  .selectAll('rect') // bars are basically nothing else than rectangles (returns an empty selection here)
+  .data(barChartData) // call the data method and provide it the dataset. I.e. will put the data in waiting state for further processing
+  .enter() // will put the data in waiting state for further processing. It takes the data from the waiting state and performs the following operations on each data item.
+  .append('rect') // appends a rectangle inside the svg container
+  .attr('y', d => svgHeight - d) // the upper left point of the bar
+  .attr('height', d => d) // the height to fill the bar
+  .attr('width', barWidth - barPadding) // the width plus some padding between the bars
+  .attr('transform', (d, i) => `translate(${[barWidth * i, 0]})`); // to put the bars next to each other
+
+// Proceed on 11:16 of https://www.youtube.com/watch?v=C4t6qfHZ6Tw
