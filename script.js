90788bbef44d4a6f5a3e426fe0aa84ccf4592815
Dennis <deweis@gmx.ch>
2018-12-10 12:23:10 +0100

    Add simple example on dataset triggered p elements

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hi there..');
+/**************************************************************************************************
+  Simple Example on creating p elements based on a dataset
+**************************************************************************************************/
+const dataset = [1, 2, 3, 4, 5];
+
+/* Create a p tag for each item in dataset */
+d3.select('body') // select the body element
+  .selectAll('p') // select all p tags -> will return an empty selection as no p elements available
+  .data(dataset) // will put the data in waiting state for further processing
+  .enter() // will take the data items one by one and perform further operations on them
+  .append('p') // appends a p item for each data item
+  .text(d => d); // binds respective text inside each p item (in this case the dataitem itself)
