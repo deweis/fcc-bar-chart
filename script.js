cb2b06d1d06d032de4990047fcd60bc35e5e8cbc
Dennis <deweis@gmx.ch>
2018-12-11 10:02:49 +0100

    Fetch the data to be visualized

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* Fetch the data to be visualized */
+let fccData = {};
+
+fetch(
+  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log('data fetched');
+    fccData = myJson; //JSON.stringify(myJson);
+    console.log(fccData.data[0]);
+  });
+
 /**************************************************************************************************
   Simple Example on creating p elements based on a dataset
 **************************************************************************************************/
